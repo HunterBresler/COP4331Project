2d7012c17c25846f8f55cb4ec927aa8b53d8e268
HunterBresler <hunterwayneb@icloud.com>
2023-12-03 15:50:03 -0500

    Fixing login ig

--- shoppingcartapp/src/main/java/com/login/LogIn.java
+++ shoppingcartapp/src/main/java/com/login/LogIn.java
@@ -7,17 +7,32 @@ import javax.swing.JFrame;
 public class LogIn extends JFrame 
 {
     private Map<String, String> db = new HashMap<>(); // database for login credentials
+    private boolean correctLogIn = false; // tracks whether there has been a successful login
 
     // constructor
     public LogIn()
     {
         // Fill db
         readDB();
+        showLogInFrame();
     }
     
-    public void showFrame()
+    public void showLogInFrame()
     {
+        String user = "";
 
+        // Show frame
+
+        while(correctLogIn == false)
+        {
+            // Get correct login credentials
+            user = inputLogIn();
+        }
+
+        // Close frame
+
+        // Create user
+        createUser(user);
     }
 
     // Reads db.txt and writes it into db variable
@@ -27,11 +42,18 @@ public class LogIn extends JFrame
     }
 
     // Get input for login and return true if valid
-    public String input()
+    public String inputLogIn()
     {
         // Get input
-        checkLogIn("temp", "temp");
-        return "";
+        String user = "temp";
+
+        // check and validate login
+        if (checkLogIn("temp", "temp"))
+        {
+            correctLogIn = true;
+        }
+
+        return user;
     }
 
     // Check if login credentials match db
@@ -51,5 +73,23 @@ public class LogIn extends JFrame
     public void showError()
     {
 
+    }
+
+    public void createUser(String user)
+    {
+        // Open selection
+        String classSelection = inputClass();
+
+        // Create class based on class selection
+
+        // Open shopping cart for specified user
+    }
+
+    public String inputClass()
+    {
+        // Get selection
+        String selection = "temp";
+
+        return selection;
     }
 }
