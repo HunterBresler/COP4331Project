9f62dc42f8472dd08fe3994a5a2b2df9fd7e0f33
sirnerd030 <133157976+sirnerd030@users.noreply.github.com>
2023-12-06 18:22:33 -0500

    Update Main to open Login GUI

--- shoppingcartapp/src/main/java/com/shoppingcart/App.java
+++ shoppingcartapp/src/main/java/com/shoppingcart/App.java
@@ -1,9 +1,15 @@
 package com.shoppingcart;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.login.LogIn;
+import javax.swing.SwingUtilities;
+
+public class App {
+    public static void main(String[] args) {
+        // Use SwingUtilities.invokeLater to ensure thread safety for GUI creation
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new LogIn(); // Create an instance of the LogIn class, which will display the GUI
+            }
+        });
     }
 }
