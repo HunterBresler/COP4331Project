fb9883c4d60bb5f008d63839d682bcc7b3646147
sirnerd030 <133157976+sirnerd030@users.noreply.github.com>
2023-12-06 20:08:46 -0500

    Update Cart.java
    - Product Management by ID: Revised to manage products using their IDs instead of Product objects directly.
    - Enhanced Cart Operations: Provided methods to add, update, and remove products, as well as to retrieve cart contents and calculate the total cost.
    - Integration with Product Catalog: Adjusted to work with a product catalog for retrieving Product details based on IDs.

--- shoppingcartapp/src/main/java/com/app/Cart.java
+++ shoppingcartapp/src/main/java/com/app/Cart.java
@@ -1,6 +1,51 @@
 package com.app;
 
-public class Cart 
-{
-    
+import com.inventory.Product;
+import java.util.HashMap;
+import java.util.Map;
+
+class Cart {
+    private final Map<Integer, Integer> productQuantities = new HashMap<>();
+    private Map<Integer, Product> productCatalog; // Assuming a product catalog is available
+
+    public Cart(Map<Integer, Product> productCatalog) {
+        this.productCatalog = productCatalog;
+    }
+
+    public void addOrUpdateProduct(int productId, int quantity) {
+        productQuantities.put(productId, productQuantities.getOrDefault(productId, 0) + quantity);
+    }
+
+    public void updateProductQuantity(int productId, int quantity) {
+        if (quantity <= 0) {
+            productQuantities.remove(productId);
+        } else {
+            productQuantities.put(productId, quantity);
+        }
+    }
+
+    public void removeProduct(int productId) {
+        productQuantities.remove(productId);
+    }
+
+    public Map<Product, Integer> getCartContents() {
+        Map<Product, Integer> contents = new HashMap<>();
+        productQuantities.forEach((id, qty) -> {
+            Product product = productCatalog.get(id);
+            if (product != null) {
+                contents.put(product, qty);
+            }
+        });
+        return contents;
+    }
+
+    public double calculateTotal() {
+    return productQuantities.entrySet().stream()
+            .mapToDouble(entry -> {
+                Product product = productCatalog.get(entry.getKey());
+                return product != null ? product.getSellingPrice() * entry.getValue() : 0;
+            })
+            .sum();
+    }
 }
+    
