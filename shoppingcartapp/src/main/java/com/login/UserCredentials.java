63e44f5a6f9a2e22de4c4a73e877395c425d2e45
sirnerd030 <133157976+sirnerd030@users.noreply.github.com>
2023-12-07 21:03:23 -0500

    Create UserCredentials.java

---
+++ shoppingcartapp/src/main/java/com/login/UserCredentials.java
@@ -0,0 +1,25 @@
+package com.login;
+
+public class UserCredentials {
+    private String username;
+    private String password;
+    private String role;
+
+    public UserCredentials(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
